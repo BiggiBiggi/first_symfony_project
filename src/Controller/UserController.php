22fb298a6bdce2b46e32f20d0f7db3e997b99a3b
BiggiBiggi <frelandbenjamin99@gmail.com>
2024-06-25 15:03:49 +0200

    Add user contacts list

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,20 +8,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     // ...
-    #[Route('/user/controller')]
+    #[Route('/user/notifications')]
     public function notifications(): Response
     {
         // get the user information and notifications somehow
         $userFirstName = '...';
         $userNotifications = ['...', '...'];
+        $userContacts = ['Manon', 'Killian', 'Jeremy'];
 
         // the template path is the relative file path from `templates/`
         return $this->render('user/notifications.html.twig', [
-            // this array defines the variables passed to the template,
-            // where the key is the variable name and the value is the variable value
-            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
             'user_first_name' => $userFirstName,
             'notifications' => $userNotifications,
+            'contacts' => $userContacts,
         ]);
     }
 }
