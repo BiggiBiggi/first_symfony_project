9f6d68f34dd8fa40faee7571866cdfc0c57014b9
BiggiBiggi <frelandbenjamin99@gmail.com>
2024-06-28 15:34:06 +0200

    GOT Controller

--- src/Controller/GotController.php
+++ src/Controller/GotController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class GOTController extends AbstractController
@@ -11,8 +12,11 @@ class GOTController extends AbstractController
     #[Route('/got', name: 'app_got')]
     public function index(): Response
     {
+        $request = new Request();
+        $request->create('https://anapioficeandfire.com/api/characters/583', 'GET');
         return $this->render('got/index.html.twig', [
             'controller_name' => 'GOTController',
+            'request' => $request,
         ]);
     }
 }
